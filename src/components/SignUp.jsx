ace2d6d20a8940a0be3a0dc73559271a65b32c9e
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-05 02:55:27 +0600

    Create Users Post Api And Save USer To MongoDB

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -9,11 +9,30 @@ const SignUp = () => {
         e.preventDefault();
         const form = e.target;
         const email = form.email.value;
+        const name = form.name.value;
         const password = form.password.value;
         console.log(email, password);
         createUser(email, password)
             .then(result => {
                 console.log(result.user);
+                const createdAt = result?.user?.metadata?.creationTime
+                const newUser = {name, email, createdAt}
+
+                // Save New User Info To The Database
+                fetch('http://localhost:5000/users', {
+                    method: "POST",
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(newUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('user created to db ', data);
+                        if(data.insertedId) {
+                            console.log('user created in db')
+                        }
+                    })
             })
             .catch(error => {
                 console.log(error.message);
@@ -22,16 +41,18 @@ const SignUp = () => {
 
     return (
         <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="hero-content flex-col">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Sign Up!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+                    <h1 className="text-5xl font-bold mb-12">Sign Up!</h1>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="email" name="name" className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
