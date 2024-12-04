62bb9c3f071daaeade2022133951f8266e4f9b10
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-05 01:20:38 +0600

    cloned older repo and created firebase project

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCi7ssslT5Kcw0DRl5YeH8vf9G3_zyXV0",
+  authDomain: "coffee-store-93826.firebaseapp.com",
+  projectId: "coffee-store-93826",
+  storageBucket: "coffee-store-93826.firebasestorage.app",
+  messagingSenderId: "723014362642",
+  appId: "1:723014362642:web:9ebb54b7d156376ad39acb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './components/Layout.jsx';
+import Home from './components/Home.jsx';
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+import Signin from './components/Signin.jsx';
+import SignUp from './components/SignUp.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('http://localhost:5000/coffee')
+      },
+      {
+        path: '/addCoffee',
+        element: <AddCoffee></AddCoffee>
+      },
+      {
+        path: '/updateCoffee/:id',
+        element: <UpdateCoffee></UpdateCoffee>,
+        loader: ({ params }) => fetch(`http://localhost:5000/coffee/${params.id}`)
+      },
+      {
+        path: "/signin",
+        element: <Signin></Signin>
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
